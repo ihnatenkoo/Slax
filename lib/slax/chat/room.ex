e7e4f8b0a2cc0845ef61df7600825a136eb34a76
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-11-02 11:52:36 +0200

    Add funcs to create and update rooms, add validation schema

--- lib/slax/chat/room.ex
+++ lib/slax/chat/room.ex
@@ -1,22 +1,3 @@
-defmodule Slax.Chat do
-  alias Slax.Chat.Room
-  alias Slax.Repo
-
-  import Ecto.Query
-
-  def get_room!(id) do
-   Room |> Repo.get!(id)
-  end
-
-  def get_first_room! do
-    Repo.one!(from r in Room, limit: 1, order_by: [asc: :name])
-  end
-
-  def list_rooms do
-    Repo.all(from r in Room, order_by: [asc: :name])
-  end
-end
-
 defmodule Slax.Chat.Room do
   use Ecto.Schema
   import Ecto.Changeset
@@ -32,6 +13,39 @@ defmodule Slax.Chat.Room do
   def changeset(room, attrs) do
     room
     |> cast(attrs, [:name, :topic])
-    |> validate_required([:name, :topic])
+    |> validate_required([:name])
+    |> validate_length(:name, max: 80)
+    |> validate_format(:name,  ~r/\A[a-z0-9-]+\z/,
+      message: "can only contain lowercase letters, numbers and dashes")
+    |> validate_length(:topic, max: 200)
+  end
+end
+
+defmodule Slax.Chat do
+  alias Slax.Chat.Room
+  alias Slax.Repo
+
+  import Ecto.Query
+
+  def create_room(attrs) do
+    %Room{}
+    |> Room.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def update_room(%Room{} = room, attrs) do
+    room |> Room.changeset(attrs) |> Repo.update()
+  end
+
+  def get_room!(id) do
+   Room |> Repo.get!(id)
+  end
+
+  def get_first_room! do
+    Repo.one!(from r in Room, limit: 1, order_by: [asc: :name])
+  end
+
+  def list_rooms do
+    Repo.all(from r in Room, order_by: [asc: :name])
   end
 end
