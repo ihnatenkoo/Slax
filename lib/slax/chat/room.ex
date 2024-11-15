548e918238fa626f1263bca65f415eaa73c85902
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-11-15 12:05:06 +0200

    Show room joined status on index page

--- lib/slax/chat/room.ex
+++ lib/slax/chat/room.ex
@@ -87,6 +87,17 @@ defmodule Slax.Chat do
     |> Enum.sort_by(& &1.name)
   end
 
+  def list_rooms_with_joined(%User{} = user) do
+    query =
+      from r in Room,
+        left_join: m in RoomMembership,
+        on: r.id == m.room_id and m.user_id == ^user.id,
+        select: {r, not is_nil(m.id)},
+        order_by: [asc: :name]
+
+    Repo.all(query)
+  end
+
   def joined?(%Room{} = room, %User{} = user) do
     Repo.exists?(
       from rm in RoomMembership, where: rm.room_id == ^room.id and rm.user_id == ^user.id
