f41243ed7db8627c7a5ff9e03aef5201b167e4fe
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-11-18 11:26:49 +0200

    Implement new chat room creation form and validation logic

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -210,7 +210,18 @@ defmodule SlaxWeb.ChatRoomLive do
     </div>
     <.modal id="new-room-modal">
       <.header>New chat room</.header>
-      (Form goes here)
+      <.simple_form
+        for={@new_room_form}
+        id="room-form"
+        phx-change="validate-room"
+        phx-submit="save-room"
+      >
+        <.input field={@new_room_form[:name]} type="text" label="Name" phx-debounce />
+        <.input field={@new_room_form[:topic]} type="text" label="Topic" phx-debounce />
+        <:actions>
+          <.button phx-disable-with="Saving..." class="w-full">Save</.button>
+        </:actions>
+      </.simple_form>
     </.modal>
     """
   end
@@ -356,6 +367,7 @@ defmodule SlaxWeb.ChatRoomLive do
       socket
       |> assign(rooms: rooms, timezone: timezone, users: users)
       |> assign(online_users: OnlineUsers.list())
+      |> assign_room_form(Chat.change_room(%Room{}))
       |> stream_configure(:messages,
         dom_id: fn
           %Message{id: id} -> "messages-#{id}"
@@ -454,6 +466,30 @@ defmodule SlaxWeb.ChatRoomLive do
     {:noreply, socket}
   end
 
+  def handle_event("validate-room", %{"room" => room_params}, socket) do
+    changeset =
+      socket.assigns.room
+      |> Chat.change_room(room_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign_room_form(socket, changeset)}
+  end
+
+  def handle_event("save-room", %{"room" => room_params}, socket) do
+    case Chat.create_room(room_params) do
+      {:ok, room} ->
+        Chat.join_room!(room, socket.assigns.current_user)
+
+        {:noreply,
+         socket
+         |> put_flash(:info, "Created room")
+         |> push_navigate(to: ~p"/rooms/#{room}")}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign_room_form(socket, changeset)}
+    end
+  end
+
   def handle_info({:new_message, message}, socket) do
     room = socket.assigns.room
 
@@ -493,6 +529,10 @@ defmodule SlaxWeb.ChatRoomLive do
 
   defp assign_message_form(socket, changeset) do
     assign(socket, :new_message_form, to_form(changeset))
+  end
+
+  defp assign_room_form(socket, changeset) do
+    assign(socket, :new_room_form, to_form(changeset))
   end
 
   defp message_timestamp(message, timezone) do
