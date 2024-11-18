633100f9c93d0b9bf7550cc291eb44046f3462b6
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2024-11-18 13:54:35 +0200

    Refactor socket handling in ChatRoomLive and ChatRoomLive.Index for improved readability

--- lib/slax_web/live/chat_room_live/index.ex
+++ lib/slax_web/live/chat_room_live/index.ex
@@ -58,13 +58,11 @@ defmodule SlaxWeb.ChatRoomLive.Index do
   def mount(_params, _session, socket) do
     rooms = Chat.list_rooms_with_joined(socket.assigns.current_user)
 
-    socket =
-      socket
-      |> assign(:page_title, "All rooms")
-      |> stream_configure(:rooms, dom_id: fn {room, _} -> "rooms-#{room.id}" end)
-      |> stream(:rooms, rooms)
-
-    {:ok, socket}
+    socket
+    |> assign(:page_title, "All rooms")
+    |> stream_configure(:rooms, dom_id: fn {room, _} -> "rooms-#{room.id}" end)
+    |> stream(:rooms, rooms)
+    |> ok()
   end
 
   def handle_event("toggle-room-membership", %{"id" => id}, socket) do
@@ -73,6 +71,6 @@ defmodule SlaxWeb.ChatRoomLive.Index do
       |> Chat.get_room!()
       |> Chat.toggle_room_membership(socket.assigns.current_user)
 
-    {:noreply, stream_insert(socket, :rooms, {room, joined?})}
+    stream_insert(socket, :rooms, {room, joined?}) |> noreply()
   end
 end
